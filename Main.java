4c7d285a37b8f0cb360c7731a34effb7297eeb33
“Thembelihle <thembelihlecheryl5@gmail.com>
2025-04-26 22:19:29 +0200

    Change Main.java in main branch

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello NetBeans. Welcome to version control using Git");
+        System.out.println("Hello from the main branch!");
     }
 }
